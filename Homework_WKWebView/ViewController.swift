144bc8bdd8be98a10f6ea843f82357361f54889e
Vlad Krupen <air7360@gmail.com>
2023-12-24 12:56:24 +0300

    Initial commit

---
+++ Homework_WKWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework_WKWebView
+//
+//  Created by Vlad on 24.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
+
